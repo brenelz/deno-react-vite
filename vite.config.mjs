28c10ad615b1a20fded817157a454672e68ec403
Brenelz <brenleydueck@gmail.com>
2022-09-26 21:28:24 -0500

    Import npm:react

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'npm:vite'
 import react from 'npm:@vitejs/plugin-react'
 
+import 'npm:react';
+import 'npm:react-dom';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
